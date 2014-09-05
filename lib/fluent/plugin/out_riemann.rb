ff3de8c8d13e8e1b61771a2fc5d023517cc7c02d
mirwan <mirwan666@gmail.com>
2014-09-05 14:38:09 +0200

    Fix typo on port parameter name expected

--- lib/fluent/plugin/out_riemann.rb
+++ lib/fluent/plugin/out_riemann.rb
@@ -4,7 +4,7 @@ class Fluent::RiemannOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('riemann', self)
 
   config_param :host, :string, :default => '127.0.0.1'
-  config_param :post, :integer, :default => 5555
+  config_param :port, :integer, :default => 5555
   config_param :timeout, :integer, :default => 5
   config_param :protocol, :string, :default => 'tcp'
   config_param :service, :string, :default => nil
